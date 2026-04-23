a6528700d1912baaf9cdbc580750d96a09925dda
uwam29-spec <uwam.29@gmail.com>
2026-04-23 23:56:38 +0700

    Add files via upload

---
+++ notpad/script.js
@@ -0,0 +1,11 @@
+var nama = "Muhamad Ridwan";
+var nim = "15240650";
+var umur = 20;
+var jurusan = "Teknik Informatika";
+var storytelling = "Perkenalkan nama saya Muhamad Ridwan. Saya adalah mahasiswa Universitas Bina Sarana Informatika yang sedang belajar pemrograman web menggunakan HTML dan JavaScript.";
+
+document.getElementById("nama").innerHTML = "Nama : " + nama;
+document.getElementById("nim").innerHTML = "NIM : " + nim;
+document.getElementById("umur").innerHTML = "Umur : " + umur + " Tahun";
+document.getElementById("jurusan").innerHTML = "Jurusan : " + jurusan;
+document.getElementById("storytelling").innerHTML = "Story telling : " + story;
