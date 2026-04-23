c6fdf748b12f3648c3a53f3eda34fea429b78111
uwam29-spec <uwam.29@gmail.com>
2026-04-23 19:46:43 +0700

    Add files via upload

---
+++ biodata diri/script.js
@@ -0,0 +1,27 @@
+var nama = "Muhamad Ridwan";
+var nim = "15240650";
+var tempatLahir = "bekasi";
+var tanggalLahir = "29 juli  2005";
+var jenisKelamin = "Laki-laki";
+var alamat = "kp.bubulak desa bojongkulur kec.gunung putri kab.bogor";
+var email = "mhmmdr906@gmai.com";
+var nohp = "08988835226";
+var hobi = "bermain sepak bola";
+var pendidikan = "S1";
+var kampus = "Universitas Bina Sarana Informatika";
+var prodi = "Informatika";
+
+
+document.getElementById("hasil").innerHTML =
+"Nama : " + nama + "<br>" +
+"NIM : " + nim + "<br>" +
+"Tempat Lahir : " + tempatLahir + "<br>" +
+"Tanggal Lahir : " + tanggalLahir + "<br>" +
+"Jenis Kelamin : " + jenisKelamin + "<br>" +
+"Alamat : " + alamat + "<br>" +
+"Email : " + email + "<br>" +
+"No HP : " + nohp + "<br>" +
+"Hobi : " + hobi + "<br>" +
+"Pendidikan : " + pendidikan + "<br>" +
+"Kampus : " + kampus + "<br>" +
+"Program Studi : " + prodi;
